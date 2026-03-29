cf6abef6183d3be62fbaef8ac99b2760e14d3c91
jeremiandrade <jeremiandrade04@gmail.com>
2026-03-29 13:07:06 +0200

    add exercise n6

--- ex-js-array-methods-main/snack-6/script.js
+++ ex-js-array-methods-main/snack-6/script.js
@@ -11,4 +11,22 @@ const zucchine = [
   { type: 'Calabrese', weight: 14, length: 4 },
 ];
 
+
+const courgetteLength = zucchine.filter((courgette)=>{
+  return courgette.length >= 15
+})
+
+const courgetteShort = zucchine.filter((courgette)=>{
+  return courgette.length < 15
+})
+
+console.log(courgetteLength,courgetteShort);
+
+
+
+
+
+
+
+
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
