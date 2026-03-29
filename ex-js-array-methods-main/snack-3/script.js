b5722ad700ad439f1c96819519975baf3c55bd04
jeremiandrade <jeremiandrade04@gmail.com>
2026-03-29 11:06:22 +0200

    add exercise n3

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,17 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const newNumber = numbers.map((number)=>{
+    return number + 1 ;
+})
+console.log(newNumber);
+
+
+
+
+
+
+
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
