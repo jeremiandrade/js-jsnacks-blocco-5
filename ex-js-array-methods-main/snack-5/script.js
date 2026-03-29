d497f52aa9b08d1e458d12eb05fca77b16999070
jeremiandrade <jeremiandrade04@gmail.com>
2026-03-29 12:07:40 +0200

    add exercise n5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,14 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+const pari = nums.filter((number)=>{
+if (number%2===0) {
+    return true;
+} return false;
+
+})
+console.log(pari);
+
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
