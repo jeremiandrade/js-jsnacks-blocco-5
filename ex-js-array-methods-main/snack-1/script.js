6be28188ccea5ea45cdcc07d032de4d6787fda44
jeremiandrade <jeremiandrade04@gmail.com>
2026-03-29 10:27:53 +0200

    add ex n1

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,8 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
+console.log(names);
+
+//chiedere se e giusto l'esercizio
+
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
